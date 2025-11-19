3b14d0be7a66f2f20123a22f4993702a707b53dc
Robin Charlet <contact@robincharlet.fr>
2025-11-19 12:07:40 +0100

    feat: add DebugEventDispatcherDecorator and update service definitions
    - Introduced `DebugEventDispatcherDecorator` to log dispatched events for debugging
    - Applied `AsDecorator` attribute to `DebugEventDispatcherDecorator` for service decoration
    - Updated `OutputtingXpCalculator` to use `AsDecorator` for `XpCalculatorInterface`
    - Refactored service definitions in `services.yaml` to utilize proper decorators

---
+++ src/Decorator/DebugEventDispatcherDecorator.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Decorator;
+
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+#[AsDecorator('event_dispatcher')]
+class DebugEventDispatcherDecorator implements EventDispatcherInterface
+{
+    public function __construct(Private readonly EventDispatcher $eventDispatcher)
+    {
+    }
+
+    public function dispatch(object $event, string $eventName = null): object
+    {
+        dump('--------------------');
+        dump('Dispatching event: ' . $event::class);
+        dump('--------------------');
+
+        return $this->eventDispatcher->dispatch($event, $eventName);
+    }
+
+    public function addListener(string $eventName, $listener, int $priority = 0): void
+    {
+        $this->eventDispatcher->addListener($eventName, $listener, $priority);
+    }
+
+    public function addSubscriber(EventSubscriberInterface $subscriber): void
+    {
+        $this->eventDispatcher->addSubscriber($subscriber);
+    }
+
+    public function removeListener(string $eventName, $listener): void
+    {
+        $this->eventDispatcher->removeListener($eventName, $listener);
+    }
+
+    public function removeSubscriber(EventSubscriberInterface $subscriber): void
+    {
+        $this->eventDispatcher->removeSubscriber($subscriber);
+    }
+
+    public function getListeners(string $eventName = null): array
+    {
+        return $this->eventDispatcher->getListeners($eventName);
+    }
+
+
+
+    public function getListenerPriority(string $eventName, $listener): ?int
+    {
+        return $this->eventDispatcher->getListenerPriority($eventName, $listener);
+    }
+
+    public function hasListeners(string $eventName = null): bool
+    {
+        return $this->eventDispatcher->hasListeners($eventName);
+    }
+}
